24c5bbd76672405fe35f1c9836490f8abf470ba9
Mohamed Sayed <mohamed.ahmed056@eng-st.cu.edu.eg>
2026-03-01 18:39:51 +0200

    Update app.py

--- task01-signal-viewer-sbeg205_spring26_team15-main/app.py
+++ task01-signal-viewer-sbeg205_spring26_team15-main/app.py
@@ -551,4 +551,4 @@ def stock_history():
     except Exception as e:
         return jsonify({'success': False, 'error': str(e)})
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, use_reloader=False)
